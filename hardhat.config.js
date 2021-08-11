9fd8347bba30fb483ad7f361635ddf27c639a325
jalil <jalil@1001.digital>
2021-08-11 17:00:51 +0200

    Project setup

---
+++ hardhat.config.js
@@ -0,0 +1,46 @@
+require('@nomiclabs/hardhat-etherscan')
+require('@nomiclabs/hardhat-waffle')
+require('hardhat-gas-reporter')
+require('dotenv').config()
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: {
+    version: "0.8.0",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 1000,
+      },
+    },
+  },
+  networks: {
+    ropsten: {
+      url: `https://ropsten.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts: [`0x${process.env.PRIVATE_KEY}`],
+    },
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts: [`0x${process.env.PRIVATE_KEY}`],
+    },
+    hardhat: {
+      chainId: 1337,
+      forking: {
+        url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
+      },
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+  mocha: {
+    timeout: 10000,
+  },
+  gasReporter: {
+    currency: 'USD',
+    gasPrice: 45,
+    coinmarketcap: process.env.COINMARKETCAP_API_KEY,
+  },
+};
