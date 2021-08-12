0aaa5badace9f113f51cc4c5d913df066240752b
jalil <jalil@1001.digital>
2021-08-13 01:00:58 +0200

    Remove unnecessary grouping

--- test/WithSaleStartExample.js
+++ test/WithSaleStartExample.js
@@ -32,39 +32,37 @@ describe('WithSaleStartExample', async () => {
     })
   })
 
-  describe('Public Sale', () => {
-    describe('SaleStart', () => {
-      it('Should expose the saleStart time', async () => {
-        expect(await contract.saleStart()).to.equal(saleStart)
-      })
+  describe('SaleStart', () => {
+    it('Should expose the saleStart time', async () => {
+      expect(await contract.saleStart()).to.equal(saleStart)
+    })
 
-      it('Should be able to change sale start before the sale has started', async () => {
-        await contract.connect(owner).setSaleStart(saleStart + TWO_MINUTES)
-      })
+    it('Should be able to change sale start before the sale has started', async () => {
+      await contract.connect(owner).setSaleStart(saleStart + TWO_MINUTES)
+    })
 
-      it('Should not be able to change sale start after the sale has started', async () => {
-        await contract.connect(owner).setSaleStart(saleStart - TWO_MINUTES)
-        await expect(contract.connect(owner).setSaleStart(saleStart + TWO_MINUTES))
-          .to.be.revertedWith('Sale has already started')
-      })
+    it('Should not be able to change sale start after the sale has started', async () => {
+      await contract.connect(owner).setSaleStart(saleStart - TWO_MINUTES)
+      await expect(contract.connect(owner).setSaleStart(saleStart + TWO_MINUTES))
+        .to.be.revertedWith('Sale has already started')
+    })
 
-      it('Should not mint if sale hasn\'t started yet', async () => {
-        await expect(contract.connect(buyer).mint())
-          .to.be.revertedWith('Sale hasn\'t started yet')
-      })
+    it('Should not mint if sale hasn\'t started yet', async () => {
+      await expect(contract.connect(buyer).mint())
+        .to.be.revertedWith('Sale hasn\'t started yet')
+    })
 
-      it('Should allow mint if sale has started', async () => {
-        await contract.connect(owner).setSaleStart(saleStart - TWO_MINUTES)
-        await expect(contract.connect(buyer).mint())
-          .to.emit(contract, 'Transfer')
-      })
+    it('Should allow mint if sale has started', async () => {
+      await contract.connect(owner).setSaleStart(saleStart - TWO_MINUTES)
+      await expect(contract.connect(buyer).mint())
+        .to.emit(contract, 'Transfer')
+    })
 
-      it('Should emit SaleStartChanged when the sale start changes', async () => {
-        const time = saleStart + TWO_MINUTES
-        await expect(contract.connect(owner).setSaleStart(time))
-          .to.emit(contract, 'SaleStartChanged')
-          .withArgs(time)
-      })
+    it('Should emit SaleStartChanged when the sale start changes', async () => {
+      const time = saleStart + TWO_MINUTES
+      await expect(contract.connect(owner).setSaleStart(time))
+        .to.emit(contract, 'SaleStartChanged')
+        .withArgs(time)
     })
   })
 })
