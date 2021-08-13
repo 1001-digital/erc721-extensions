ed4331904084562e66d04f87540d32732167db5e
jalil <jalil@1001.digital>
2021-08-13 02:21:51 +0200

    Expose a getter for the available token count

--- contracts/WithLimitedSupply.sol
+++ contracts/WithLimitedSupply.sol
@@ -20,18 +20,26 @@ abstract contract WithLimitedSupply {
         _maxSupply = maxSupply_;
     }
 
-
     /// @dev Get the max Supply
+    /// @return the maximum token count
     function maxSupply() public view returns (uint256) {
         return _maxSupply;
     }
 
     /// @dev Get the current token count
+    /// @return the created token count
     function tokenCount() public view returns (uint256) {
         return _tokenCount.current();
     }
 
+    /// @dev Check whether tokens are still available
+    /// @return the available token count
+    function availableTokenCount() public view returns (uint256) {
+        return maxSupply() - tokenCount();
+    }
+
     /// @dev Increment the token count and fetch the latest count
+    /// @return the next token id
     function nextToken() internal virtual ensureAvailability returns (uint256) {
         _tokenCount.increment();
 
@@ -40,7 +48,7 @@ abstract contract WithLimitedSupply {
 
     /// @dev Check whether tokens are still available
     modifier ensureAvailability() {
-        require(tokenCount() < maxSupply(), "No more tokens available");
+        require(availableTokenCount() > 0, "No more tokens available");
         _;
     }
 }
