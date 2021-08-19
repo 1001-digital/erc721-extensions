a917ec6baf486f5f1c5a32f1a38a1ada54811765
jalil <jalil@1001.digital>
2021-08-19 23:10:20 +0200

    Fix indentation

--- contracts/examples/WithSaleStartExample.sol
+++ contracts/examples/WithSaleStartExample.sol
@@ -6,19 +6,19 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "./../WithSaleStart.sol";
 
 contract WithSaleStartExample is ERC721, WithSaleStart {
-  uint256 private _tokenId = 0;
+    uint256 private _tokenId = 0;
 
-  constructor(
-    uint256 time
-  )
-    ERC721("MyToken", "MT")
-    WithSaleStart(time)
-  {}
+    constructor(
+        uint256 time
+    )
+        ERC721("MyToken", "MT")
+        WithSaleStart(time)
+    {}
 
-  function mint () external afterSaleStart returns (uint256) {
-    _tokenId++;
-    _safeMint(msg.sender, _tokenId);
+    function mint () external afterSaleStart returns (uint256) {
+        _tokenId++;
+        _safeMint(msg.sender, _tokenId);
 
-    return _tokenId;
-  }
+        return _tokenId;
+    }
 }
