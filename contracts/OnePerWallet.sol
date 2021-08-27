b6675847d40c2fb0a7a571e8eb2e45f3b2676197
jalil <jalil@1001.digital>
2021-08-27 17:32:27 +0200

    Account for 0-indexed collections

--- contracts/OnePerWallet.sol
+++ contracts/OnePerWallet.sol
@@ -25,7 +25,10 @@ abstract contract OnePerWallet is ERC721 {
     /// @param owner the address of the owner
     /// @dev Get the the token of an owner
     function tokenOf(address owner) public view virtual returns (uint256) {
-        return _ownedToken[owner];
+        require(_ownedToken[owner] > 0, "No token for this account.");
+
+        // We subtract 1 as we added 1 to account for 0-index based collections
+        return _ownedToken[owner] - 1;
     }
 
     /// Store `_ownedToken` instead of `_balances`.
@@ -35,7 +38,8 @@ abstract contract OnePerWallet is ERC721 {
     function _mint(address to, uint256 tokenId) internal virtual override onePerWallet(to) {
         super._mint(to, tokenId);
 
-        _ownedToken[to] = tokenId;
+        // We add one to account for 0-index based collections
+        _ownedToken[to] = tokenId + 1;
     }
 
     /// Track transfers in `_ownedToken` instead of `_balances`
@@ -47,6 +51,7 @@ abstract contract OnePerWallet is ERC721 {
         super._transfer(from, to, tokenId);
 
         _ownedToken[from] = 0;
-        _ownedToken[to] = tokenId;
+        // We add one to account for 0-index based collections
+        _ownedToken[to] = tokenId + 1;
     }
 }
